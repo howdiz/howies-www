249caaee1ce3edd971aa65fdbc7b87d0abf89ae5
Howie Ross <howie.ross@moovweb.com>
2020-10-04 19:57:16 -0400

    sapper w/ webpack
    updated index, layout, template

---
+++ src/routes/index.svelte
@@ -0,0 +1,89 @@
+<style>
+	h1, figure {
+		text-align: center;
+		margin: 0 auto;
+	}
+
+	h1 {
+		font-size: 2.8em;
+		text-transform: uppercase;
+		font-weight: 700;
+		margin: 0 0 0.5em 0;
+	}
+
+	figure {
+		margin: 0 0 1em 0;
+	}
+
+	img {
+		width: 100%;
+		max-width: 400px;
+		margin: 0 0 1em 0;
+	}
+
+	p {
+		margin: 1em auto;
+	}
+	.fa {
+		font-size: xx-large;
+	}
+	.icons i {
+		padding-left: 5px;
+		padding-right: 5px;
+
+	}
+
+	@media (min-width: 480px) {
+		h1 {
+			font-size: 4em;
+		}
+	}
+</style>
+
+<svelte:head>
+	<title>Howie Ross's World Wide Web Page</title>
+</svelte:head>
+
+<h1>Hello, I’m Howie Ross</h1>
+
+<figure>
+	<img alt='Success Kid' src='goodone.jpg'>
+	<div class="icons">
+		<a href="https://twitter.com/howdiz">
+			<i class="fa fa-twitter"></i>
+		</a>
+		<a href="https://www.linkedin.com/in/howardross">
+			<i class="fa fa-linkedin"></i>
+		</a>
+		<a href="https://www.instagram.com/howdiz/">
+			<i class="fa fa-instagram"></i>
+		</a>
+		<a href="https://github.com/howdiz">
+			<i class="fa fa-github"></i>
+		</a>
+	</div>
+</figure>
+
+<p>
+	I’m the Director of Solutions Engineering at
+	<a href="https://www.moovweb.com/">Moovweb</a>.
+
+	I’ve been building stuff on the internet for over 15 years and building
+	and leading teams for about half of that time.
+</p>
+<p> 
+	I have worked for startups, agencies, and financial services companies, building websites, 
+	apps, e-commerce systems, and, more recently cloud platforms.
+	You can find my resume <a href="https://docs.google.com/document/d/1ETSnhJWz0tnomekjgxsyehLep-nFzp05W_-OdqPzhwg/edit?usp=sharing">here</a>.
+</p>
+<p>	
+	I've also lectured for the UX/IxD Masters
+	program at Thomas Jefferson University and spoke at conferences, meetups, and boot camps.
+</p>
+<p>
+	I used to have a lot of hobbies like brewing beer, organize meetups including
+	<a href="https://www.meetup.com/CTO-School-Philadelphia/">CTO School
+	Philly</a>, and be on charitable organization boards like the
+	<a href="http://www.awesomefoundation.org/en/chapters/philadelphia" >
+	Philly Awesome Foundation</a>. Now, I'm happily the father of twin boys, and I'm very into coffee.
+</p>
